64637fdd5f81427f1ee3abcbeea69f1d95e83809
Muhammadmuavia <khanmuavia480@gmail.com>
2023-02-16 11:51:57 +0500

    AL-QURAN

---
+++ resources/views/backend/layouts/partials/sidebar.blade.php
@@ -0,0 +1,100 @@
+ <!-- sidebar menu area start -->
+ @php
+     $usr = Auth::guard('admin')->user();
+ @endphp
+ <div class="sidebar-menu" id="myNav">
+    <div class="sidebar-header">
+        <div class="logo">
+            <a href="{{ route('admin.dashboard') }}">
+                <h2 class="text-white"><span style="color: #FDC155;">Al</span>  Quran</h2> 
+            </a>
+        </div>
+    </div>
+    <div class="main-menu">
+        <div class="menu-inner">
+            <nav>
+                <ul class="metismenu" id="menu">
+
+                    @if ($usr->can('dashboard.view'))
+                    <li class=>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                        <ul class="collapse">
+                            <li class="{{ Route::is('admin.dashboard') ?  : '' }}"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                        </ul>
+                    </li>
+                    @endif
+
+                    @if ($usr->can('role.create') || $usr->can('role.view') ||  $usr->can('role.edit') ||  $usr->can('role.delete'))
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-tasks"></i><span>
+                            Roles & Permissions
+                        </span></a>
+                        <ul class="collapse {{ Route::is('admin.roles.create') || Route::is('admin.roles.index') || Route::is('admin.roles.edit') || Route::is('admin.roles.show') ? 'in' : '' }}">
+                            @if ($usr->can('role.view'))
+                                <li class="{{ Route::is('admin.roles.index')  || Route::is('admin.roles.edit') ? 'active' : '' }}"><a href="{{ route('admin.roles.index') }}">All Roles</a></li>
+                            @endif
+                            @if ($usr->can('role.create'))
+                                <li class="{{ Route::is('admin.roles.create')  ? 'active' : '' }}"><a href="{{ route('admin.roles.create') }}">Create Role</a></li>
+                            @endif
+                        </ul>
+                    </li>
+                    @endif
+
+                    
+                    @if ($usr->can('admin.create') || $usr->can('admin.view') ||  $usr->can('admin.edit') ||  $usr->can('admin.delete'))
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-user"></i><span>
+                            Admins
+                        </span></a>
+                        <ul class="collapse {{ Route::is('admin.admins.create') || Route::is('admin.admins.index') || Route::is('admin.admins.edit') || Route::is('admin.admins.show') ? 'in' : '' }}">
+                            
+                            @if ($usr->can('admin.view'))
+                                <li class="{{ Route::is('admin.admins.index')  || Route::is('admin.admins.edit') ? 'active' : '' }}"><a href="{{ route('admin.admins.index') }}">All Admins</a></li>
+                            @endif
+
+                            @if ($usr->can('admin.create'))
+                                <li class="{{ Route::is('admin.admins.create')  ? 'active' : '' }}"><a href="{{ route('admin.admins.create') }}">Create Admin</a></li>
+                            @endif
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-user"></i><span>
+                        MADARIS
+                        </span></a>
+                        <ul class="collapse {{ Route::is('admin.admins.create') || Route::is('admin.admins.index') || Route::is('admin.admins.edit') || Route::is('admin.admins.show') ? 'in' : '' }}">
+                            
+                            @if ($usr->can('admin.view'))
+                                <li class="{{ Route::is('admin.admins.index')  || Route::is('admin.admins.edit') ? 'active' : '' }}"><a href="{{ route('admin.admins.index') }}">All MADARIS</a></li>
+                            @endif
+
+                            @if ($usr->can('admin.create'))
+                                <li class="{{ Route::is('admin.admins.create')  ? 'active' : '' }}"><a href="{{ route('admin.admins.create') }}">Create Madrsah</a></li>
+                            @endif
+                        </ul>
+                    </li>
+                    @endif
+
+                </ul>
+                
+               
+            </nav>
+
+
+            <script>
+        function openNav() {
+  document.getElementById("myNav").style.width = "21%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}    
+
+
+</script>
+        </div>
+    </div>
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" style="font-size: larger; display:flex;margin: top 1%; margin-left:9%"><b>x</b></a>
+</div>
+<button onclick="openNav()" ><b>XY</b></button>
+<!-- sidebar menu area end -->
